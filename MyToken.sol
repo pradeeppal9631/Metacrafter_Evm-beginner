e163957dd358cfd6a31eacead69bb2c87c92f710
pradeeppal9631 <pp725891@gmail.com>
2024-08-27 20:36:25 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,28 +1,44 @@
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the address by that amount.
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the address.
+    5. Lastly, your burn function should have conditionals to make sure the balance of the account is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.20;
-
+pragma solidity 0.8.18;
 contract MyToken {
+
+    // public variables here
     string public tokenName;
-    string public tokenSymbol;
-    uint256 public tokenTotalSupply;
-    mapping(address => uint256) public tokenBalances;
+    string public tokenAbbrv;
+    uint256 public totalSupply = 0;
 
-    constructor(string memory _tokenName, string memory _tokenSymbol, uint256 _tokenTotalSupply) {
-        tokenName = _tokenName;
-        tokenSymbol = _tokenSymbol;
-        tokenTotalSupply = _tokenTotalSupply;
-        tokenBalances[msg.sender] = _tokenTotalSupply; // Assign all tokens to contract deployer initially
+    // mapping variable here
+    mapping(address => uint256) public balance;
+
+    // constructor to initialize the token details
+    constructor(string memory _name, string memory _abbrv) {
+        tokenName = _name;
+        tokenAbbrv = _abbrv;
     }
 
-    function mint(address account, uint256 value) public {
-        tokenTotalSupply += value;
-        tokenBalances[account] += value;
+    // mint function
+    function mint(address addr, uint256 _value) public {
+        totalSupply += _value;
+        balance[addr] += _value;                                  //increased balance
     }
 
-    function burn(address account, uint256 value) public{
-        require(tokenBalances[account] >= value, "Insufficient balance");
-        tokenTotalSupply -= value;
-        tokenBalances[account] -= value;
+    // burn function
+    function burn(address addr, uint256 _value) public {
+        require(balance[addr] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balance[addr] -= _value;                                                //decreased balance
     }
 }
